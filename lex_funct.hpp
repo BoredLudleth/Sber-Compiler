fbeed2ddc8046233c98173924d01b1fa15321cb3
BoredLudleth <mozgovoy.ka@gmail.com>
2024-03-06 01:39:20 +0300

    v1.5 new string and comment

--- lex_funct.hpp
+++ lex_funct.hpp
@@ -2,13 +2,7 @@
 #define LEX_FUNCT_HPP
 
 #include <iostream>
-#include "error_codes.hpp"
-
-// for /* */
-int readAttachedComment ();
-
-// for --
-void readNotAttachedComment ();
+#include "settings.hpp"
 
 //template function for messages for all things
 void print_msg (const char* my_msg, char* yytext = nullptr);
--- settings.hpp
+++ settings.hpp
@@ -1,10 +1,16 @@
-#ifndef ERROR_CODES_HPP
-#define ERROR_CODES_HPP
+#ifndef SETTINGS_HPP
+#define SETTINGS_HPP
 
+//string settings
+#define MAX_STRING_LENGTH 255
+
+//errors in string
 #define ERROR_NULL_IN_STRING 0x11
 #define ERROR_EOF_IN_STRING 0X12
 
+//errors in comment
 #define ERROR_EOF_IN_COMMENT 0X21
 #define ERROR_NULL_IN_COMMENT 0X22
+
 
 #endif
